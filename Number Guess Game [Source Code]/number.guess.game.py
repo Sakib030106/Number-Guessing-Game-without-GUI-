6355fb82fcc6abdf2c5e580c66995d483dabedc1
Sadman Sakib <82639119+Sakib030106@users.noreply.github.com>
2021-04-28 16:23:49 +0600

    Add files via upload

---
+++ Number Guess Game [Source Code]/number.guess.game.py
@@ -0,0 +1,33 @@
+# This is a number guessing game.
+
+# Introduction:
+name = input('Hi! Welcome to this game. Please enter your name: ')
+c = name.title()
+print('Welcome,', c)
+
+import random     # Importing turtle module:
+
+num = random.randint(1, 1000)
+attempts = 0
+
+# Engine:
+while True:
+    try:
+        input_num = input('Guess the number (between 1 to 1000): ')
+        input_num = int(input_num)
+        attempts += 1
+
+        if input_num == num:
+            print('Yes, your guess is correct! ')
+            break
+        elif input_num > num:
+            print('Incorrect! Please try to guess a smaller number. ')
+        else:
+            print('Incorrect! Please try to guess a larger number. ')
+    except ValueError:
+        print('Invalid Input. Try again. ')
+        continue
+print('You tried', attempts, 'times to find the number. ')
+
+# End:
+end = input('Thank you for playing this game! See you soon.')
